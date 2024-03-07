387504c580a965363fb754ed24a492bcb743e724
ajitkumarpal1 <66736656+ajitkumarpal1@users.noreply.github.com>
2024-03-07 16:16:09 +0530

    aupdate all component

--- assets/scr/main.js
+++ assets/scr/main.js
@@ -1,29 +1,3 @@
-function main(email) {
-    let result;
-
-    const atIndex = email.indexOf('@');
-    const charactersBeforeAt = atIndex >= 3;
-
-    const isValidEmail = charactersBeforeAt && atIndex !== -1 && atIndex < email.length - 1;
-
-    const lastDotIndex = email.lastIndexOf('.');
-    const charactersAfterDot = lastDotIndex - (atIndex + 1) >= 2;
-
-    if (charactersBeforeAt && isValidEmail && charactersAfterDot) {
-        result = `Welcome ${email} to our site`;
-    } else {
-        result = 'Invalid email';
-    }
-
-    return result;
-}
-/* brack */
-
-console.log(main('xyz@gmail.com'));  
-console.log(main('xzgmail.com'));
-
-
-
 // Get the scrolling container element
 const scrollingContainer = document.querySelector('.scrolling-container');
 
